fba4d635d4b9c0b14e159dac0e014632ce7433cf
Damian Olszewski <damianolszewski25@gmail.com>
2023-03-10 12:58:15 +0100

    register done

--- src/components/myRegi/MyRegi.jsx
+++ src/components/myRegi/MyRegi.jsx
@@ -11,6 +11,43 @@ import {
   saveUserAction,
 } from "../../redux/actions";
 export default function MyRegi() {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const [name, setName] = useState();
+  const [lastName, setLastName] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const regIn = {
+      name: name,
+      lastName: lastName,
+      email: email,
+      password: password,
+    };
+    try {
+      const options = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(regIn),
+      };
+      const response = await fetch(
+        `http://localhost:3002/users/register`,
+        options
+      );
+      if (response.ok) {
+        setTimeout(() => {
+          navigate("/");
+        }, 1000);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="center">
       <Container
@@ -30,21 +67,21 @@ export default function MyRegi() {
                 fontFamily: "Open Sans, sans serif",
                 fontSize: "16px",
               }}
-              //   onSubmit={handleSubmit}
+              onSubmit={handleSubmit}
             >
-              <Form.Group controlId="formBasicEmail">
+              <Form.Group controlId="formName">
                 <Form.Label>Name</Form.Label>
                 <Form.Control
-                  //   onChange={(e) => setEmail(e.target.value)}
+                  onChange={(e) => setName(e.target.value)}
                   type="text"
                   placeholder="Your name"
                   required
                 />
               </Form.Group>
-              <Form.Group controlId="formBasicEmail">
+              <Form.Group controlId="formLastName">
                 <Form.Label>Last Name</Form.Label>
                 <Form.Control
-                  //   onChange={(e) => setEmail(e.target.value)}
+                  onChange={(e) => setLastName(e.target.value)}
                   type="text"
                   placeholder="Your last name"
                   required
@@ -53,7 +90,7 @@ export default function MyRegi() {
               <Form.Group controlId="formBasicEmail">
                 <Form.Label>Email address</Form.Label>
                 <Form.Control
-                  //   onChange={(e) => setEmail(e.target.value)}
+                  onChange={(e) => setEmail(e.target.value)}
                   type="email"
                   placeholder="Enter email"
                   required
@@ -65,7 +102,7 @@ export default function MyRegi() {
                 <Form.Control
                   type="password"
                   placeholder="Enter Password"
-                  //   onChange={(e) => setPassword(e.target.value)}
+                  onChange={(e) => setPassword(e.target.value)}
                   required
                 />
               </Form.Group>
@@ -78,9 +115,9 @@ export default function MyRegi() {
                 }}
                 variant="primary"
                 type="submit"
-                // onClick={(e) => handleSubmit(e)}
+                onClick={(e) => handleSubmit(e)}
               >
-                Login
+                Register
               </Button>
               <Link to={"/"}>
                 <Button className="mt-2" variant="link">
